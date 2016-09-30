1a027feb5dea2e3ee04d9691431389454fb56663
Scarlett Clark <sgclark@kde.org>
2016-09-29 18:12:00 -0700

    More vlc conversion from bash script to new template.

---
+++ functions/build_contribs.sh
@@ -0,0 +1,14 @@
+sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get --yes --force-yes install  gcc-6 g++-6
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-6
+
+#Fix broken crap.
+sed -i '/$(UPDATE_AUTOCONFIG)/d' /app/src/vlc/contrib/src/dca/rules.mak
+sed -i '/$(UPDATE_AUTOCONFIG)/d' /app/src/vlc/contrib/src/dvbpsi/rules.mak
+
+cd /app/src/vlc/contrib
+mkdir native
+cd native
+../bootstrap --prefix=/app/usr
+make -j 8
