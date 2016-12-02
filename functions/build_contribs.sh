6d0b6df017ce005e0271149e83049d5815363397
Scarlett Clark <sgclark@kde.org>
2016-12-02 04:59:21 -0700

    Fix gcc update-alternatives

--- functions/build_contribs.sh
+++ functions/build_contribs.sh
@@ -1,6 +1,6 @@
 
 sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
-sudo apt-get -y update
+sudo apt-get update
 sudo apt-get --yes --force-yes install  gcc-6 g++-6
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-6
 
